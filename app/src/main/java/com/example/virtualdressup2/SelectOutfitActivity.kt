aa4f2e8e0663fdfc4f4f50a7e1da909e29be6a8a
Erwin <cpan518@gmail.com>
2024-04-28 22:56:04 -0700

    Added tops and bottoms garment recycler view to be displayed on SelectOutfitActivity, Added model, tops and bottoms adapter

--- app/src/main/java/com/example/virtualdressup2/SelectOutfitActivity.kt
+++ app/src/main/java/com/example/virtualdressup2/SelectOutfitActivity.kt
@@ -5,12 +5,15 @@ package com.example.virtualdressup2
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.virtualdressup2.OutfitAdapter
@@ -23,31 +26,31 @@ import com.myapp.firebase.users.UserDAO
 import com.myapp.revery.Garment
 import com.myapp.revery.ReveryAIClient
 import com.squareup.picasso.Picasso
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class SelectOutfitActivity : AppCompatActivity() {
 
-//    private lateinit var reveryAIClient: ReveryAIClient
+    //    private lateinit var reveryAIClient: ReveryAIClient
 //    private lateinit var recyclerView: RecyclerView
 //    private lateinit var newOutfitAdapter: NewOutfitAdapter
 //    private var mostRecentPosition: Int = 0
     private lateinit var binding: SelectOutfitBinding
     private lateinit var model: RecyclerItem
+//    private lateinit var tops: RecyclerItem
+//    private lateinit var bottoms: RecyclerItem
+    private lateinit var topsAdapter: GarmentTopsAdapter
+    private lateinit var bottomsAdapter: GarmentBottomsAdapter
+
 //    private val outfitList = mutableListOf<RecyclerItem>()
 
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val garmentTopsList = mutableListOf<RecyclerItem>()
+        val garmentBottomsList = mutableListOf<RecyclerItem>()
         binding = SelectOutfitBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Initialize ReveryAI client
-//        reveryAIClient = ReveryAIClient()
-
-//        binding.modelImageView.visibility = View.GONE
 
         lifecycleScope.launch {
             try {
@@ -57,15 +60,66 @@ class SelectOutfitActivity : AppCompatActivity() {
                 val avatarModels = avatar.modelID
 
                 // Assuming `model` contains the model URL
-                val modelURL = "https://media.revery.ai/revery_client_models/${avatar.modelID}/ou_aligned_transparent.png"
+                val modelURL =
+                    "https://media.revery.ai/revery_client_models/${avatar.modelID}/ou_aligned_transparent.png"
                 model = RecyclerItem(
                     titleImage = 0, // Set to 0 or any other default value since we're using titleImageURL
                     heading = "", // Add a heading if necessary
                     modelID = modelURL // Assign modelURL to modelID
                 )
+//                // Add outfits to outfitList
+                for (avatar in avatarModels) {
+                    val topsURL =
+                        "https://revery-integration-tools.s3.us-east-2.amazonaws.com/API_website/tops.jpeg"
+                    garmentTopsList.add(RecyclerItem(0, "", titleImageURL = topsURL))
+
+                }
+
+                for (avatar in avatarModels) {
+                    val bottomsURL =
+                        "https://revery-integration-tools.s3.us-east-2.amazonaws.com/API_website/bottoms.jpeg"
+                    garmentBottomsList.add(RecyclerItem(0, "", titleImageURL = bottomsURL))
+                }
+
                 // Load the image into the ImageView using Picasso
                 bindWithModelURL(model)
                 displaySelectedOutfit(model)
+
+                // Use LinearLayoutManager to display outfits in a horizontal layout
+                val topsLayoutManager = LinearLayoutManager(this@SelectOutfitActivity, LinearLayoutManager.HORIZONTAL, false)
+                binding.topsRecyclerview.layoutManager = topsLayoutManager
+
+                // Create an instance of GarmentTopsAdapter and pass in the garmentTopsList and item click listener
+                topsAdapter = GarmentTopsAdapter(garmentTopsList) { _, position ->
+                    // Display a toast message indicating the clicked outfit
+                    Toast.makeText(
+                        this@SelectOutfitActivity,
+                        "Top: ${garmentTopsList[position].titleImageURL}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                // Set the adapter for the RecyclerView
+                binding.topsRecyclerview.adapter = topsAdapter
+
+                val bottomsLayoutManager = LinearLayoutManager(this@SelectOutfitActivity, LinearLayoutManager.HORIZONTAL, false)
+                binding.bottomsRecyclerView.layoutManager = bottomsLayoutManager
+
+                // Create an instance of GarmentBottomsAdapter and pass in the garmentBottomsList and item click listener
+                bottomsAdapter = GarmentBottomsAdapter(garmentBottomsList) { _, position ->
+                    // Display a toast message indicating the clicked outfit
+                    Toast.makeText(
+                        this@SelectOutfitActivity,
+                        "Bottom: ${garmentBottomsList[position].titleImageURL}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                binding.bottomsRecyclerView.adapter = bottomsAdapter
+
+                val itemDecoration = SpacesItemDecoration(10) // replace 10 with your desired space in pixels
+                binding.topsRecyclerview.addItemDecoration(itemDecoration)
+                binding.bottomsRecyclerView.addItemDecoration(itemDecoration)
+
             } catch (e: Exception) {
                 Toast.makeText(
                     this@SelectOutfitActivity,
@@ -74,24 +128,19 @@ class SelectOutfitActivity : AppCompatActivity() {
                 ).show()
             }
 
-
         }
-
-
 
         // Fetch and display initial outfit
             // fetchAndDisplayOutfit()
 
             // Set click listener for the back button
-            findViewById<FloatingActionButton>(R.id.backButton).setOnClickListener {
-                onBackPressed() // Simulate back button press
-            }
+        binding.backButton.setOnClickListener {
+            onBackPressed()
+        }
 
-            // Set click listener for the "Save Outfit" button
-            findViewById<Button>(R.id.buttonSave).setOnClickListener {
-                // Save the selected outfit
-//            saveSelectedOutfit()
-            }
+        binding.buttonSave.setOnClickListener{
+
+        }
 
             // Display TryOnFragment
 //        supportFragmentManager.beginTransaction().replace(R.id.tryOnFragmentContainer, TryOnFragment()).commit()
@@ -108,6 +157,7 @@ class SelectOutfitActivity : AppCompatActivity() {
         // Set view to urlImage
         Picasso.get().load(outfit.modelID).into(binding.modelImageView)
     }
+
 
 //        fun fetchAndDisplayOutfit() {
 //            // Make API call to get filtered garments (outfits)
